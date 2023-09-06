2f4708f58306f0043b8c90a42ce71d464c292d31
noby1223 <nobutaka.ito@polytechs.co.jp>
2023-09-06 16:45:01 +0900

    連続ラインのコードを作成、ファイルも整理

--- fem3d/main.py
+++ fem3d/main.py
@@ -2,4 +2,4 @@ import fem3d_body_wet_sekisan_4BOX as fem4box
 
 
 
-fem4box.main_dayo()
+fem4box.main_dayo()#メモリ確認用のファイル？
--- msh_create.py
+++ msh_create.py
@@ -1,7 +1,8 @@
 import getfem as gf
 
-m = gf.Mesh("import", "gmsh", "/home/noby/fem/fem3d/msh_geo/t20rev.msh")
-m.export_to_vtk( "t20rev_ascii","ascii")
+for i in range(10):
+    m = gf.Mesh("import", "gmsh", '/home/noby/fem/fem3d/msh_geo/continuousline_test/paint_wet_mesh/paint_wet_done_' + str(i) + '.msh')
+    m.export_to_vtk( "/home/noby/fem/fem3d/msh_geo/continuousline_test/paint_vtk/test_paint_" + str(i) + ".vtk","ascii")
 
 # m = gf.Mesh("import", "gmsh", "/home/noby/fem/fem3d/boxtest.msh")
 # gf.Mesh.export_to_vtk(m, "boxtest.vtk")
